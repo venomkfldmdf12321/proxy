5d0f224f11f07ad6ea43690902259c79fa1bf87f
venomkfldmdf12321 <venomfx074@gmail.com>
2025-02-18 22:40:42 +0530

    n

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Lost?');
+});
+
 app.get('/proxy', async (req, res) => {
   const targetUrl = 'https://www.gadgets360.com/';
 
